4d6d6a193a1f98e9e75057c9ad8ce3372ad41d88
lsq1651 <670780138@qq.com>
2020-04-28 17:31:10 +0800

    添加示例接口

---
+++ src/main/java/com/great/api/modules/sys/user/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.great.api.modules.sys.user.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.great.api.modules.sys.user.entity.User;
+
+/**
+ * 用户信息表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2020-04-28 14:12:17
+ */
+public interface UserDao extends BaseMapper<User> {
+
+}
--- src/main/java/com/great/api/modules/sys/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/great/api/modules/sys/user/service/impl/UserServiceImpl.java
@@ -1,79 +1,18 @@
 package com.great.api.modules.sys.user.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.great.api.modules.sys.user.dao.UserDao;
 import com.great.api.modules.sys.user.entity.User;
-import com.great.api.modules.sys.user.mapper.UserMapper;
 import com.great.api.modules.sys.user.service.UserService;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * 用户信息表(User)表服务实现类
  *
  * @author makejava
- * @since 2020-04-28 11:09:09
+ * @since 2020-04-28 14:12:17
  */
 @Service("userService")
-public class UserServiceImpl implements UserService {
-    @Resource
-    private UserMapper userMapper;
+public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService {
 
-    /**
-     * 通过ID查询单条数据
-     *
-     * @param userId 主键
-     * @return 实例对象
-     */
-    @Override
-    public User queryById(Long userId) {
-        return this.userMapper.queryById(userId);
-    }
-
-    /**
-     * 查询多条数据
-     *
-     * @param offset 查询起始位置
-     * @param limit 查询条数
-     * @return 对象列表
-     */
-    @Override
-    public List<User> queryAllByLimit(int offset, int limit) {
-        return this.userMapper.queryAllByLimit(offset, limit);
-    }
-
-    /**
-     * 新增数据
-     *
-     * @param user 实例对象
-     * @return 实例对象
-     */
-    @Override
-    public User insert(User user) {
-        this.userMapper.insert(user);
-        return user;
-    }
-
-    /**
-     * 修改数据
-     *
-     * @param user 实例对象
-     * @return 实例对象
-     */
-    @Override
-    public User update(User user) {
-        this.userMapper.update(user);
-        return this.queryById(user.getUserId());
-    }
-
-    /**
-     * 通过主键删除数据
-     *
-     * @param userId 主键
-     * @return 是否成功
-     */
-    @Override
-    public boolean deleteById(Long userId) {
-        return this.userMapper.deleteById(userId) > 0;
-    }
 }
